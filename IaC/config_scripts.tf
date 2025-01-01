66e74b9cc0cd1c1c54e884317a53ed35ecaa12e8
subbareddyalamur1 <subbareddy.alamuru@sycamoreinformatics.com>
2025-01-01 19:58:46 +0530

    update config_scripts.tf to track changes only in windows scripts

--- IaC/config_scripts.tf
+++ IaC/config_scripts.tf
@@ -2,7 +2,10 @@ data "archive_file" "windows_scripts" {
   source_dir  = "${path.module}/scripts/windows"
   output_path = "${path.module}/windows_scripts.zip"
   type        = "zip"
-  excludes    = ["**/guacamole/*", "**/lambda/*"]
+  excludes    = ["guacamole/**/*", "lambda/**/*"]
+
+  # Add hash output to properly track changes
+  output_base64sha256 = filebase64sha256("${path.module}/windows_scripts.zip")
 }
 
 resource "aws_s3_object" "scripts_zip" {
@@ -11,3 +14,4 @@ resource "aws_s3_object" "scripts_zip" {
   source = data.archive_file.windows_scripts.output_path
   etag   = filemd5(data.archive_file.windows_scripts.output_path)
 }
+
